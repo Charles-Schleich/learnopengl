aac5c093c25ef659603dc0b2028c05012798c963
Charles Schleich <schleich.cdaniel@gmail.com>
2020-08-08 09:51:52 +0200

    Started on getting textures working, need to fix

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <learnopengl/shader.h>
+#include <stb_image/stb_image.h>
 #include <cmath>
 
 // Declarations 
@@ -46,44 +47,87 @@ int main()
                     , "./shaders/fragment.shader");
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    //float vertices[] = {
-    // 0.5f, -0.5f, 0.0f,  // bottom right
-    //-0.5f, -0.5f, 0.0f,  // bottom left
-    // 0.0f, 0.75f, 0.0f   // peak
-    //};
+    // Triangle verticies
+    // float vertices[] = {
+    //     // positions         // colors
+    //      0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+    //     -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+    //      0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
+    // };
 
-    //unsigned int indices[] = {  // note that we start from 0!
-    //    0, 1, 2,    // first triangle
-    //};
-
+    // Rectangle
     float vertices[] = {
-        // positions         // colors
-         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
-        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
-         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
+        // positions         // colors          // Texture Coords
+         0.5f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  1.0f, 1.0f, // top right
+         0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  1.0f, 0.0f, // bottom right
+        -0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,  0.0f, 0.0f, // bottom left 
+        -0.5f,  0.5f, 0.0f,  1.0f, 1.0f, 0.0f,  0.0f, 1.0f, // top left
     };
 
 
+    unsigned int indices[] = {
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
+    };
+
+    // textures
+    float texCoors[] = {
+        0.0f, 0.0f, // lower left corner 
+        1.0f, 0.0f, // lower right corner
+        0.5f, 1.0f, // top center
+    };
     
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D,texture);
+    // set the texture wrapping/filtering options (on the currently bound texture object)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    //load texture
+    int width, height, nrChannels;
+    unsigned char *data = stbi_load("./textures/container.jpg", &width, &height, &nrChannels,0);
+    if (data){
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else {
+        std::cout<< "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+
+
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     
     // VAO: Vertex Array Object 
     // Vertex Object 
     // EBO: 
 
-    unsigned int VBO, VAO;
+    unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // co-ordinate attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+    
     // color attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
+
+    // texture attribute
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
 
     // UNBIND VBO
     glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -105,9 +149,14 @@ int main()
         // float z_pos = (sin(timeValue) / 2.0f) +0.5f;
         //glUniform1f(vertexColorLocation, z_pos);
 
+        // glBindVertexArray(VAO);
+        // glDrawArrays(GL_TRIANGLES,0,3);
+ 
+        glBindTexture(GL_TEXTURE_2D, texture);
         glBindVertexArray(VAO);
-        //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-        glDrawArrays(GL_TRIANGLES,0,3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+
         //glDrawElements(GL_TRIANGLES, sizeof(indices), GL_UNSIGNED_INT, 0);
         // are you really reading my code right now ? 
 
